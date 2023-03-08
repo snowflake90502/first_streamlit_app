1a8c1773845fd2223d4970b8a2b34dff5e402769
snowflake90502 <127261294+snowflake90502@users.noreply.github.com>
2023-03-07 18:29:45 -0600

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3')
+streamlit.text('kale')
+streamlit.text('Egg')
