447e3d6fccb3b486b911820749ed47969e8fcac9
snowflake90502 <127261294+snowflake90502@users.noreply.github.com>
2023-03-15 01:27:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,9 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
+
+
+streamlit.write('Thanks for adding ', add_my_fruit)
+                
+#This will not work correctly, but just go with it for now
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
